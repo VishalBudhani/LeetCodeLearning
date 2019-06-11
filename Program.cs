16a19567d55fd988bc2c47f87e6fb55213f575fe
VishalBudhani <vmbudhani1718@gmail.com>
2019-06-10 23:29:21 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution os = new Solution();
+            Console.WriteLine(os.IsPalindrome(-11));
+            Console.ReadKey();
+        }
+    }
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            if (x == 0)
+            {
+                return true;
+            }
+            int originalNumber = x;
+            int reverseNumber = 0;
+            while (x > 0)
+            {
+                reverseNumber = (reverseNumber * 10) + x % 10;
+                x = x / 10;
+            }
+            return reverseNumber == originalNumber;
+        }
+    }
+}
